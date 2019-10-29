69eaa24eccad51fc8fb0b6744bade5c3aa9f34af
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2019-10-29 12:15:15 -0400

    Add readn

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,6 @@
 #include <utils.h>
 #include <string.h>
+#include <unistd.h>
 
 void array_shift_left(void *array_start, void *array_end, size_t size)
 {
@@ -11,4 +12,28 @@ void array_shift_left(void *array_start, void *array_end, size_t size)
         strncpy(start, start + size, size);
         start += size;
     }
+}
+
+int readn(int fd, char *buf, size_t n)
+{
+    int count, rc;
+    while (n--)
+    {
+        if ((rc = read(fd, buf, 1)) == 1)
+        {
+            if (*buf == '\n')
+                break;
+            else
+            {
+                buf++;
+                count++;
+            }
+        }
+        else if (rc == 0)
+            break;
+        else
+            return -1;
+    }
+    *buf = '\0';
+    return count;
 }
